d23659731b2e8abc5ae54445734c95a650357568
Osama Awad <osamaawd818@gmail.com>
2020-03-10 19:18:10 +0200

    Download the project

---
+++ app/src/main/java/com/osamaawad/graduationproject_tawasi/StartPage.java
@@ -0,0 +1,14 @@
+package com.osamaawad.graduationproject_tawasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+}
