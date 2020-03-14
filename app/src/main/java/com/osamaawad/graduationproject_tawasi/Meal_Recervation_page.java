2dcc0447470c6904a4d8465fbf9c858066dd8b4c
Osama Awad <61984878+Osamaawad818@users.noreply.github.com>
2020-03-14 19:42:16 +0200

    download the project

---
+++ app/src/main/java/com/osamaawad/graduationproject_tawasi/Meal_Recervation_page.java
@@ -0,0 +1,14 @@
+package com.osamaawad.graduationproject_tawasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Meal_Recervation_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal__recervation_page);
+    }
+}
