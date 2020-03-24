fbb905ecbd58f0a3eecfc33e75a285b01827a8d6
Osama Awad <osamaawd818@gmail.com>
2020-03-25 01:37:09 +0200

    download the project

---
+++ app/src/main/java/com/osamaawad/graduationproject_tawasi/DeletIt.java
@@ -0,0 +1,14 @@
+package com.osamaawad.graduationproject_tawasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeletIt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delet_it);
+    }
+}
